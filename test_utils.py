132803bb303c362027cf76a455f3729c28be4d24
Marcio Mazza <marciomazza@gmail.com>
2017-08-18 12:51:26 -0300

    Add time representation util

--- test_utils.py
+++ test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 
-from utils import find_value_intervals, get_first_and_last_from_iterator
+from utils import (find_value_intervals, get_first_and_last_from_iterator,
+                   timerepr)
 
 
 def test_first_and_last_from_iter():
@@ -17,3 +18,20 @@ ___ = 999
 ])
 def test_find_value_intervals(data, intervals):
     assert find_value_intervals(data, ___) == intervals
+
+
+def to_milliseconds(hour, minute, second, millis):
+    minute += hour * 60
+    second += minute * 60
+    millis += second * 1000
+    return millis
+
+
+@pytest.mark.parametrize('millis, output', [
+    (to_milliseconds(1000, 3, 42, 23), '1000:03:42.023'),
+    (to_milliseconds(12, 3, 42, 23), '12:03:42.023'),
+    (to_milliseconds(0, 3, 0, 999), '03:00.999'),
+    (to_milliseconds(0, 0, 0, 0), '00:00.000'),
+])
+def test_timerepr(millis, output):
+    assert timerepr(millis) == output
--- utils.py
+++ utils.py
@@ -20,3 +20,17 @@ def find_value_intervals(data, value):
 
     value_groups = groupby(enumerate(data), lambda (i, v): v == value)
     return [interval(g) for k, g in value_groups if k]
+
+
+def timerepr(millis):
+    '''based on
+    https://www.darklaunch.com/2009/10/06/python-time-duration-human-friendly-timestamp'''
+
+    seconds, millis = divmod(millis, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    if hours:
+        return '{:02d}:{:02d}:{:02d}.{:03d}'.format(hours, minutes, seconds, millis)
+    else:
+        return '{:02d}:{:02d}.{:03d}'.format(minutes, seconds, millis)
