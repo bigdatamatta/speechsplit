0fe26018b6607d9d2bd39403fa73c540af4c4900
Marcio Mazza <marciomazza@gmail.com>
2017-08-22 12:42:03 -0300

    Add exhaustive fragmentation by silence

--- silence.py
+++ silence.py
@@ -3,12 +3,15 @@ from itertools import product
 
 from pydub.silence import detect_silence
 
+SILENCE_LEVELS = list(product(range(-42, -33), range(500, 200, -100)))
 
-def detect_silence_and_audible(audio_segment, min_silence_len=500,
-                               silence_thresh=-42):
+
+def detect_silence_and_audible(audio_segment, level=0):
     '''Splits audios segments in chunks separated by silence.
     Keep the silence in the beginning of each chunk, as possible,
     and ignore silence after the last chunk.'''
+
+    silence_thresh, min_silence_len = SILENCE_LEVELS[level]
 
     silent_ranges = detect_silence(audio_segment,
                                    min_silence_len, silence_thresh)
@@ -21,18 +24,38 @@ def detect_silence_and_audible(audio_segment, min_silence_len=500,
     if silent_ranges[-1][1] is not len_seg:
         silent_ranges.append((len_seg, len_seg))
 
-    return [(start, end, start_next)
+    return [[start, end, start_next, level]
             for (start, end), (start_next, __) in zip(silent_ranges,
                                                       silent_ranges[1:])]
 
 
-SILENCE_LEVELS = list(product(range(-42, -33), range(500, 200, -100)))
+def seek_split(audio, level=0):
+    for level in range(level, len(SILENCE_LEVELS)):
+        chunks = detect_silence_and_audible(audio, level)
+        if len(chunks) > 1:
+            return chunks
+    else:
+        return chunks
 
 
-def seek_split(audio):
-    for min_silence_len, silence_thresh in SILENCE_LEVELS:
-        split = detect_silence_and_audible(audio,
-                                           min_silence_lenf=min_silence_len,
-                                           silence_thresh=silence_thresh)
-        if len(split) > 1:
-            return split, min_silence_len, silence_thresh
+def fragment(audio, max_audible_allowed_size=10000):
+    chunks = detect_silence_and_audible(audio)
+    while(True):
+        for pos, (silence_start, start, end, level) in enumerate(chunks):
+            if (end - start > max_audible_allowed_size and
+                    level + 1 < len(SILENCE_LEVELS)):
+                subsplit = seek_split(audio[start:end], level + 1)
+                # shift all chunks by "start"
+                subsplit = [[s + start, i + start, e + start, l]
+                            for s, i, e, l in subsplit]
+                # attach previous silence to first chunk of subsplit
+                subsplit[0][0] = silence_start
+                # last end must be the silence start of next global chunk
+                if pos + 1 < len(chunks):
+                    chunks[pos + 1][0] = subsplit[-1][2]
+                chunks[pos:pos + 1] = subsplit
+                break
+        else:
+            # there's nothing more to split
+            break
+    return chunks
--- test_silence.py
+++ test_silence.py
@@ -1,26 +1,26 @@
 import pytest
 from mock import MagicMock, patch
 
-from silence import detect_silence_and_audible
+from silence import SILENCE_LEVELS, detect_silence_and_audible
 
 
 @pytest.mark.parametrize('silence_ranges, split_ranges', [
 
     ([(0, 100)], []),     # whole segment is silence
-    ([], [(0, 0, 100)]),  # whole segment is audible
+    ([], [[0, 0, 100]]),  # whole segment is audible
 
     # start: silence, end: silence (last silence ignored)
-    ([(0, 10), (20, 100)], [(0, 10, 20)]),
-    ([(0, 10), (20, 30), (40, 100)], [(0, 10, 20), (20, 30, 40)]),
+    ([(0, 10), (20, 100)], [[0, 10, 20]]),
+    ([(0, 10), (20, 30), (40, 100)], [[0, 10, 20], [20, 30, 40]]),
     # start: silence, end: audible
-    ([(0, 10)], [(0, 10, 100)]),
-    ([(0, 10), (20, 30)], [(0, 10, 20), (20, 30, 100)]),
+    ([(0, 10)], [[0, 10, 100]]),
+    ([(0, 10), (20, 30)], [[0, 10, 20], [20, 30, 100]]),
     # start: audible, end: silence (last silence ignored)
-    ([(20, 100)], [(0, 0, 20)]),
+    ([(20, 100)], [[0, 0, 20]]),
     # start: audible, end: audible
-    ([(10, 20)], [(0, 0, 10), (10, 20, 100)]),
+    ([(10, 20)], [[0, 0, 10], [10, 20, 100]]),
 ])
-def test_split_on_silence(silence_ranges, split_ranges):
+def test_detect_silence_and_audible(silence_ranges, split_ranges):
 
     audio_stub = MagicMock()
     audio_stub.__len__.return_value = 100
@@ -28,6 +28,10 @@ def test_split_on_silence(silence_ranges, split_ranges):
     with patch('silence.detect_silence',
                return_value=silence_ranges) as mock_detect_silence:
 
-        assert split_ranges == detect_silence_and_audible(
-            audio_stub, 999, -99)
-        mock_detect_silence.assert_called_once_with(audio_stub, 999, -99)
+        level = 0
+        silence_thresh, min_silence_len = SILENCE_LEVELS[level]
+
+        assert split_ranges == [
+            d[:3] for d in detect_silence_and_audible(audio_stub, level)]
+        mock_detect_silence.assert_called_once_with(
+            audio_stub, min_silence_len, silence_thresh)
